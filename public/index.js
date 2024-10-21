693bfa9e8ef21fb9c325f162f20835f8aaff7c6d
g45t345rt <49697006+g45t345rt@users.noreply.github.com>
2024-10-21 11:06:34 -0400

    add testnet control

--- public/index.js
+++ public/index.js
@@ -8,16 +8,25 @@ async function load() {
 load();
 
 function generate_wallet() {
-  const key_pair = new KeyPair('mainnet');
+  const network = document.getElementById("select_network").value;
+
+  const mainnet = network === "mainnet";
+  if (mainnet) {
+    document.getElementById("lbl_testnet").classList.add("hidden");
+  } else {
+    document.getElementById("lbl_testnet").classList.remove("hidden");
+  }
+
+  const key_pair = new KeyPair(mainnet);
   const addr = key_pair.address();
   const private_key = key_pair.secret();
   const seed = key_pair.seed("en");
 
   set_address_qrcode(addr);
   set_private_key_qrcode(private_key);
+
   document.getElementById("address_value").textContent = addr;
   document.getElementById("private_key_value").textContent = private_key;
-  console.log(seed)
   document.getElementById("seed_value_line_1").textContent = seed.splice(0, 12).join(" ");
   document.getElementById("seed_value_line_2").textContent = seed.splice(0, 13).join(" ");
 }
@@ -52,11 +61,14 @@ function set_private_key_qrcode(value) {
   document.getElementById("qrcode_private_key_img").setAttribute('xlink:href', qr.toDataURL());
 }
 
-document.getElementById("generate").addEventListener('click', async () => {
+document.getElementById("btn_generate").addEventListener('click', async () => {
   generate_wallet();
 });
 
-document.getElementById("bg_select").addEventListener('change', (e) => {
-  console.log(e.target.value)
+document.getElementById("select_network").addEventListener('change', (e) => {
+  generate_wallet();
+});
+
+document.getElementById("select_bg").addEventListener('change', (e) => {
   document.getElementById("bg_img").setAttribute("xlink:href", e.target.value);
 });
